b89000efd370986343cddc445d6b46c66a487f38
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-04-11 10:50:33 +0100

    feat: Add new poll/question

---
+++ src/components/NewPoll.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleAddQuestion } from "../actions/questions";
+
+class NewPoll extends Component {
+  state = {
+    optionOne: "",
+    optionTwo: "",
+  };
+
+  isSubmitButtonActivated = () => {
+    const { optionOne, optionTwo } = this.state;
+
+    return optionOne && optionTwo;
+  };
+
+  handleOptionOneInput = (e) => {
+    let text = e.target.value;
+
+    this.setState({ optionOne: text });
+  };
+
+  handleOptionTwoInput = (e) => {
+    let text = e.target.value;
+
+    this.setState({ optionTwo: text });
+  };
+
+  handleSubmitButton = (e) => {
+    e.preventDefault();
+    const { optionOne, optionTwo } = this.state;
+    const { dispatch } = this.props;
+
+    dispatch(handleAddQuestion({ optionOne, optionTwo })).then(() => {
+      this.setState({ optionOne: "", optionTwo: "" });
+    });
+  };
+
+  render() {
+    const { optionOne, optionTwo } = this.state;
+
+    return (
+      <div>
+        <div>
+          <h2 className="ui icon center aligned header">
+            <i aria-hidden="true" className="question circular icon"></i>
+            <div className="content">Would you rather...</div>
+          </h2>
+        </div>
+        <form className="ui form">
+          <div className="ui placeholder segment">
+            {isLoading && (
+              <div className="ui active transition visible inverted dimmer">
+                <div className="content">
+                  <div className="ui text loader">Loading</div>
+                </div>
+              </div>
+            )}
+            <div className="ui very relaxed two column grid">
+              <div className="column">
+                <div className="field">
+                  <label>Option 1 </label>
+                  <div className="ui input">
+                    <input
+                      type="text"
+                      value={optionOne}
+                      placeholder="your option here"
+                      onChange={this.handleOptionOneInput}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="column">
+                <div className="field">
+                  <label className="">Option 2 </label>
+                  <div className="ui input">
+                    <input
+                      type="text"
+                      value={optionTwo}
+                      placeholder="your option here"
+                      onChange={this.handleOptionTwoInput}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="ui vertical divider">OR</div>
+          </div>
+          <br />
+          <div>
+            <button
+              className="container ui button positive"
+              onClick={this.handleSubmitButton}
+              disabled={!this.isSubmitButtonActivated()}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect()(NewPoll);
