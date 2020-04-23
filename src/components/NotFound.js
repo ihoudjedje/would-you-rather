769384e84688315c446378e04ba7759513ac1824
ilyeSudo <houdjedje.ilyes@gmail.com>
2020-04-23 14:23:43 +0100

    fix: Route to 404 when entring a non-valid questionId in url, and to question otherwise

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -5,14 +5,14 @@ function NotFound() {
   const location = useLocation();
 
   return (
-    <h2 className="ui icon header">
-      <i aria-hidden="true" className="unlinkify icon"></i>
+    <div>
+      <h1 className="ui icon header">
+        <i aria-hidden="true" className="unlinkify icon"></i>
+      </h1>
       <h1>404</h1>
-      Not match for {location.pathname}
-      <br />
-      <br />
+      <h3>Not match for {location.pathname}</h3>
       <div className="sub header">Please make sure you enter a valid URL</div>
-    </h2>
+    </div>
   );
 }
 
--- src/components/Poll/index.js
+++ src/components/Poll/index.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
 import Question from "./Question";
 import NotFound from "../NotFound";
+import { connect } from "react-redux";
 
 class Poll extends Component {
   state = {};
   render() {
-    return typeof this.props.location.state === "undefined" ? (
+    const { validQuestionId, questionType, match } = this.props;
+
+    return !validQuestionId ? (
       <NotFound />
     ) : (
       <Question
-        qId={this.props.location.state.qId}
-        questionType={this.props.location.state.questionType}
+        qId={match.params.id}
+        questionType={questionType}
         source={"poll"}
       />
     );
   }
 }
 
-export default Poll;
+function mapStateToProps({ questions, users, authedUser }, { match }) {
+  return {
+    validQuestionId: typeof questions[match.params.id] !== "undefined",
+    questionType: Object.keys(users[authedUser].answers).includes(
+      match.params.id
+    )
+      ? "answred"
+      : "unanswred",
+  };
+}
+
+export default connect(mapStateToProps)(Poll);
